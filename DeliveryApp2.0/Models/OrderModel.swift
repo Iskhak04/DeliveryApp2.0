9d28f63777adc31bed47fdba28ac6d9ba159cd80
Iskhak <izhtnv04@gmail.com>
2022-11-20 14:43:18 +0600

    createVC done

---
+++ DeliveryApp2.0/Models/OrderModel.swift
@@ -0,0 +1,16 @@
+//
+//  OrderModel.swift
+//  DeliveryApp2.0
+//
+//  Created by Iskhak Zhutanov on 20/11/22.
+//
+
+struct OrderModel {
+    var name: String
+    var from: String
+    var toWhere: String
+    var width: Double
+    var height: Double
+    var mass: Double
+    var comment: String
+}
